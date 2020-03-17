29b518f7865f57c2cec30d3f89d06d66a92252f6
maximlev03 <maximlev01234@gmail.com>
2020-03-17 20:48:25 +0200

    fixed test

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+b
+2src/main/java/ua/lviv/iot/ReturnAllWithHyphen.java,e\5\e5d6b782830dabd144a9c76830ea71629aeb2e7c
--- src/test/java/ua/lviv/iot/ReturnAllWithHyphenTest.java
+++ src/test/java/ua/lviv/iot/ReturnAllWithHyphenTest.java
@@ -1,18 +1,20 @@
 package ua.lviv.iot;
 
-import org.junit.jupiter.api.*;
-import ua.lviv.iot.ReturnAllWithHyphen;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReturnAllWithHyphenTest {
 
     @Test
-    void regex() {
+    void testSearchHyphens() {
         String input = "No one believed Hikaru when he said he was (self taught/self-taught) because his skills necessitated the presence of a teacher.\n" +
                 "Jean promised to drop the boys off at the (railroad/rail-road) station.\n" +
                 "Roy and Riza were very tired after the (three hour-long/three-hour long) PTA meeting.\n" +
@@ -25,7 +27,7 @@ class ReturnAllWithHyphenTest {
             resultedWords.addAll(ReturnAllWithHyphen.wordsValidation(scanner.nextLine()));
         }
         Collections.sort(resultedWords);
-
+        assertEquals(Arrays.asList("five-point", "five-point", "hour-long", "rail-road","self-taught", "three-hour "), resultedWords);
         for (String s: resultedWords) {
             System.out.println(s);
         }
